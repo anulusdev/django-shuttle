c8ccecaf59bb6ebb78c190e63af4cb99480d2540
AnulusDev <khalidalhazan@gmail.com>
2025-09-17 15:05:29 +0100

    add some changes

--- servigo/settings.py
+++ servigo/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-=su5+nkn5#&)d4ckt@#-8cfo%&j)4@rdkvmoyf@een%lo!f4b3"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
